ecc1536877b15acf85e89e69b9c5445205d1529f
hayeon02 <jhy414800@cu.ac.kr>
2025-04-30 19:42:49 +0900

    [GEN-629] 수집 데이터 그래프 출력

--- analytics_data_organize.py
+++ analytics_data_organize.py
@@ -28,56 +28,55 @@ class AnalyticsDataOrganize(QWidget):
 
         self.open_file()
 
-    # def open_file(self):
-    #     for i in range(len(self.file_name)):
-    #         file_path = os.path.join(self.path, self.file_name[i])
-    #         with open(file_path, 'rb') as f:
-    #             while chunk := f.read(self.record_size):
-    #                 unpacked = struct.unpack(self.struct_format, chunk)
-    #                 self.data_select(unpacked)
-    #
-    #         self.find_mode()
 
     def open_file(self):
+        count = 0
         for i in range(len(self.file_name)):
+            self.buffer.clear()
             load_data = SensorBinaryFileHandler(self.file_name[i]).load_frames()
-            print(load_data)
+            count += 1
+            self.data_select(load_data, count)
 
-    # def data_select(self, unpacked):
-    #     if self.x == 0 and self.y == 0:
-    #         pass
-    #     elif self.x == 0 and self.y == 1:
-    #         weight = unpacked[1:10]
-    #
-    #         if sum(weight) == 0 or all(weight[i] != 0 for i in self.loc):
-    #             weight_total = sum(weight)
-    #             data = [weight_total, unpacked[10], unpacked[12]]
-    #
-    #             self.group_by_port(data)
-    #
-    # def group_by_port(self, data):
-    #     weight = data[0]
-    #     loc = data[1].decode('utf-8').rstrip('\x00')
-    #     value = data[2]
-    #
-    #     if loc not in self.buffer:
-    #         self.buffer[loc] = Queue()
-    #
-    #     self.buffer[loc].put((weight, value))
-    #
-    # def find_mode(self):
-    #     data = defaultdict(lambda: defaultdict(list))
-    #
-    #     for loc, q in self.buffer.items():
-    #         while not q.empty():
-    #             weight, value = q.get()
-    #             data[loc][weight].append(value)
-    #
-    #     for loc, weight_dict in data.items():
-    #         self.total_common[loc] = {}
-    #         for weight, values in weight_dict.items():
-    #             counter = Counter(values)
-    #             most_common_value = counter.most_common(1)[0][0]
-    #             self.total_common[loc][weight] = most_common_value
-    #
-    #     AnalyticsGraph(self.total_common)
+            self.find_mode()
+
+    def data_select(self, load_data, count):
+        for data in load_data:
+            for sensor in data.sensors:
+                weight = data.experiment.weights
+                location = sensor.location.name
+                distance = sensor.distance
+
+                if sum(weight) == 0 or all(weight[i] != 0 for i in self.loc):
+                    weight_total = sum(weight)
+                    data_list = [weight_total, location, distance]
+
+                    self.group_by_port(data_list, count)
+
+    def group_by_port(self, data_list, count):
+        weight = data_list[0]
+        loc = data_list[1]
+        value = data_list[2]
+
+        buffer_name = loc + "_" + str(count)
+
+        if buffer_name not in self.buffer:
+            self.buffer[buffer_name] = Queue()
+
+        self.buffer[buffer_name].put((weight, value))
+
+    def find_mode(self):
+        data = defaultdict(lambda: defaultdict(list))
+
+        for loc, q in self.buffer.items():
+            while not q.empty():
+                weight, value = q.get()
+                data[loc][weight].append(value)
+
+        for loc, weight_dict in data.items():
+            self.total_common[loc] = {}
+            for weight, values in weight_dict.items():
+                counter = Counter(values)
+                most_common_value = counter.most_common(1)[0][0]
+                self.total_common[loc][weight] = most_common_value
+
+        AnalyticsGraph(self.total_common)
--- analytics_graph.py
+++ analytics_graph.py
@@ -1,14 +1,33 @@
-from PyQt5.QtWidgets import QMainWindow, QDockWidget, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QDockWidget, QWidget, QVBoxLayout, QLabel, QHBoxLayout, QCheckBox, QSplitter
 from PyQt5.QtCore import Qt
 import pyqtgraph as pg
+
 
 class AnalyticsGraph(QDockWidget):
     def __init__(self, data):
         super().__init__()
         self.data = data
+        self.cb_list = []
+        self.plot_items = {}
+
+        self.loc_map = {
+            'BOTTOM_RIGHT': 'red',
+            'TOP_LEFT': 'green',
+            'TOP_RIGHT': 'yellow',
+            'BOTTOM_LEFT': 'orange'
+        }
 
         self.graph_widget = pg.PlotWidget()
-        self.setWidget(self.graph_widget)
+
+        self.check_graph_layout = QWidget()
+        self.box_layout = QVBoxLayout()
+        self.check_graph_layout.setLayout(self.box_layout)
+
+        splitter = QSplitter(Qt.Horizontal)
+        splitter.addWidget(self.graph_widget)
+        splitter.addWidget(self.check_graph_layout)
+
+        self.setWidget(splitter)
 
         self.graph_data()
 
@@ -20,4 +39,30 @@ class AnalyticsGraph(QDockWidget):
                 weight_data.append(weight)
                 value_data.append(value)
 
-            self.graph_widget.plot(weight_data, value_data, name=loc)
+            base_loc = '_'.join(loc.split('_')[:2])
+            color = self.loc_map.get(base_loc, 'black')
+
+            plot_item = self.graph_widget.plot(weight_data, value_data, pen=pg.mkPen(color='black', width=2), name=loc)
+
+            self.plot_items[loc] = {
+                'plot': plot_item,
+                'color': color
+            }
+
+            self.check_loc(loc)
+
+    def check_loc(self, loc):
+        if loc not in [cb.text() for cb in self.cb_list]:
+            cb = QCheckBox(loc)
+            cb.stateChanged.connect(self.check_graph)
+            self.box_layout.addWidget(cb)
+            self.cb_list.append(cb)
+
+    def check_graph(self):
+        for cb in self.cb_list:
+            loc = cb.text()
+            if loc in self.plot_items:
+                if cb.isChecked():
+                    self.plot_items[loc]['plot'].setPen(pg.mkPen(self.plot_items[loc]['color'], width=2))
+                else:
+                    self.plot_items[loc]['plot'].setPen(pg.mkPen('black', width=2))
