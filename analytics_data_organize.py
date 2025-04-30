75988a17a6cdb95d8b85b4fc74cfc33ab8a9b4d0
hayeon02 <jhy414800@cu.ac.kr>
2025-04-30 16:54:54 +0900

    [GEN-629]수집 데이터 읽기 수정중

--- analytics_data_organize.py
+++ analytics_data_organize.py
@@ -7,15 +7,12 @@ from PyQt5.QtWidgets import QWidget, QMainWindow, QDockWidget
 from collections import defaultdict, Counter
 
 from analytics_graph import AnalyticsGraph
+from datainfo import SensorBinaryFileHandler
 
 
 class AnalyticsDataOrganize(QWidget):
     def __init__(self, path, x, y, file_name, loc):
         super().__init__()
-        self.struct_format = '<d9H16sBHHH'
-        self.record_size = struct.calcsize(self.struct_format)
-
-        # analytics_data에서 전달하는 값들
         self.path = path
         self.x = x
         self.y = y
@@ -31,51 +28,56 @@ class AnalyticsDataOrganize(QWidget):
 
         self.open_file()
 
+    # def open_file(self):
+    #     for i in range(len(self.file_name)):
+    #         file_path = os.path.join(self.path, self.file_name[i])
+    #         with open(file_path, 'rb') as f:
+    #             while chunk := f.read(self.record_size):
+    #                 unpacked = struct.unpack(self.struct_format, chunk)
+    #                 self.data_select(unpacked)
+    #
+    #         self.find_mode()
+
     def open_file(self):
         for i in range(len(self.file_name)):
-            file_path = os.path.join(self.path, self.file_name[i])
-            with open(file_path, 'rb') as f:
-                while chunk := f.read(self.record_size):
-                    unpacked = struct.unpack(self.struct_format, chunk)
-                    self.data_select(unpacked)
+            load_data = SensorBinaryFileHandler(self.file_name[i]).load_frames()
+            print(load_data)
 
-            self.find_mode()
-
-    def data_select(self, unpacked):
-        if self.x == 0 and self.y == 0:
-            pass
-        elif self.x == 0 and self.y == 1:
-            weight = unpacked[1:10]
-
-            if sum(weight) == 0 or all(weight[i] != 0 for i in self.loc):
-                weight_total = sum(weight)
-                data = [weight_total, unpacked[10], unpacked[12]]
-
-                self.group_by_port(data)
-
-    def group_by_port(self, data):
-        weight = data[0]
-        loc = data[1].decode('utf-8').rstrip('\x00')
-        value = data[2]
-
-        if loc not in self.buffer:
-            self.buffer[loc] = Queue()
-
-        self.buffer[loc].put((weight, value))
-
-    def find_mode(self):
-        data = defaultdict(lambda: defaultdict(list))
-
-        for loc, q in self.buffer.items():
-            while not q.empty():
-                weight, value = q.get()
-                data[loc][weight].append(value)
-
-        for loc, weight_dict in data.items():
-            self.total_common[loc] = {}
-            for weight, values in weight_dict.items():
-                counter = Counter(values)
-                most_common_value = counter.most_common(1)[0][0]
-                self.total_common[loc][weight] = most_common_value
-
-        AnalyticsGraph(self.total_common)
+    # def data_select(self, unpacked):
+    #     if self.x == 0 and self.y == 0:
+    #         pass
+    #     elif self.x == 0 and self.y == 1:
+    #         weight = unpacked[1:10]
+    #
+    #         if sum(weight) == 0 or all(weight[i] != 0 for i in self.loc):
+    #             weight_total = sum(weight)
+    #             data = [weight_total, unpacked[10], unpacked[12]]
+    #
+    #             self.group_by_port(data)
+    #
+    # def group_by_port(self, data):
+    #     weight = data[0]
+    #     loc = data[1].decode('utf-8').rstrip('\x00')
+    #     value = data[2]
+    #
+    #     if loc not in self.buffer:
+    #         self.buffer[loc] = Queue()
+    #
+    #     self.buffer[loc].put((weight, value))
+    #
+    # def find_mode(self):
+    #     data = defaultdict(lambda: defaultdict(list))
+    #
+    #     for loc, q in self.buffer.items():
+    #         while not q.empty():
+    #             weight, value = q.get()
+    #             data[loc][weight].append(value)
+    #
+    #     for loc, weight_dict in data.items():
+    #         self.total_common[loc] = {}
+    #         for weight, values in weight_dict.items():
+    #             counter = Counter(values)
+    #             most_common_value = counter.most_common(1)[0][0]
+    #             self.total_common[loc][weight] = most_common_value
+    #
+    #     AnalyticsGraph(self.total_common)
