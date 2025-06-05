184382584da0bb99fc4196fbdf1f8be23ae341f8
Junhyeon_Jo <colorful105@cu.ac.kr>
2025-06-05 19:38:23 +0900

    [GEN-602] refValue 데이터 평균값으로 받게 설정 - 기존 refValue의 경우 조건에 만족하는 경우 초기값으로 설정하게 진행 - 해당 조건이 맞는다고 해서 초기값으로 쓰기에는 불안정성이 존재하기에 50ms동안 받은 센서 데이터의 평균으로 설정함

--- Algorithm/RefValueGenerator_COG.py
+++ Algorithm/RefValueGenerator_COG.py
@@ -6,17 +6,24 @@ class COGRefValGenerator(RefValueGenerator):
         super().__init__()
         print('COGMassEstimation Refer Value Generator')
         self.initCheck = True
-
+        self.ti_start = 0
+        self.ti_end = 50
+        self.tl_buf = []
+        self.bl_buf = []
+        self.tr_buf = []
+        self.br_buf = []
     def calRefValue(self, input: SensorFrame):
-        if self.initCheck == True:
-            # 센서 값 추출
-            laser_values = [
-                input.get_sensor_data(SENSORLOCATION.TOP_LEFT).distance,
-                input.get_sensor_data(SENSORLOCATION.BOTTOM_LEFT).distance,
-                input.get_sensor_data(SENSORLOCATION.TOP_RIGHT).distance,
-                input.get_sensor_data(SENSORLOCATION.BOTTOM_RIGHT).distance,
-            ]
-            if all(v not in [-1, 0] for v in laser_values):
-                self.initCheck = False
-                self._refValue = laser_values
+        if self.initCheck:
+            self.tl_buf.append(input.get_sensor_data(SENSORLOCATION.TOP_LEFT).distance)
+            self.bl_buf.append(input.get_sensor_data(SENSORLOCATION.BOTTOM_LEFT).distance)
+            self.tr_buf.append(input.get_sensor_data(SENSORLOCATION.TOP_RIGHT).distance)
+            self.br_buf.append(input.get_sensor_data(SENSORLOCATION.BOTTOM_RIGHT).distance)
+            self.ti_start += 1
 
+            if self.ti_start >= self.ti_end:
+                self.initCheck = False
+                avg_tl = sum(self.tl_buf) / len(self.tl_buf)
+                avg_bl = sum(self.bl_buf) / len(self.bl_buf)
+                avg_tr = sum(self.tr_buf) / len(self.tr_buf)
+                avg_br = sum(self.br_buf) / len(self.br_buf)
+                self._refValue = [int(avg_tl), int(avg_bl), int(avg_tr), int(avg_br)]
